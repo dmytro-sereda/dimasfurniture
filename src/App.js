faedb58d089a6c799dd9dc8eb13d3418fd043de1
kingdimidze-code <sereda21d@gmail.com>
2021-08-09 12:24:06 +0300

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import { connect } from "react-redux";
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { createStructuredSelector } from "reselect";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
-import AboutPage from "./pages/about/about.component";
-import ShopPage from "./pages/shop/shop.component";
 import Cart from "./components/cart/cart.component";
 import Footer from "./components/footer/footer.component";
 
@@ -18,6 +16,8 @@ import { fetchCollectionsStart } from "./redux/shop/shop.actions";
 import { selectIsLoading } from "./redux/shop/shop.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 
+const ShopPage = lazy(() => import("./pages/shop/shop.component"));
+const AboutPage = lazy(() => import("./pages/about/about.component"));
 const HomePageWithSpinner = WithSpinner(HomePage);
 
 const App = ({ fetchCollectionsStart, checkUserSession, isLoading }) => {
@@ -31,13 +31,15 @@ const App = ({ fetchCollectionsStart, checkUserSession, isLoading }) => {
       <Header />
       <Cart />
       <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => <HomePageWithSpinner isLoading={isLoading} />}
-        />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/about" component={AboutPage} />
+        <Suspense fallback={<Spinner />}>
+          <Route
+            exact
+            path="/"
+            render={() => <HomePageWithSpinner isLoading={isLoading} />}
+          />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/about" component={AboutPage} />
+        </Suspense>
       </Switch>
       <Footer />
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import { withRouter } from "react-router";
 import { ShopPageContainer } from "./shop.styles";
 
-import CollectionOverview from "../../components/collection-overview/collection-overview.component";
-import CollectionPage from "../collection/collection.component";
+import Spinner from "../../components/spinner/spinner.component";
+
+const CollectionOverview = lazy(() =>
+  import("../../components/collection-overview/collection-overview.component")
+);
+const CollectionPage = lazy(() => import("../collection/collection.component"));
 
 const ShopPage = ({ match }) => {
   return (
     <ShopPageContainer>
-      <Route exact path={match.path} component={CollectionOverview} />
-      <Route
-        exact
-        path={`${match.path}/:collectionId`}
-        component={CollectionPage}
-      />
+      <Suspense fallback={<Spinner />}>
+        <Route exact path={match.path} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </Suspense>
     </ShopPageContainer>
   );
 };
